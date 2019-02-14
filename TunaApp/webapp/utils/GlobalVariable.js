568d2694e8b894b402faae9faa4192362dd39e05
rajagupta20 <37621211+rajagupta20@users.noreply.github.com>
2019-02-14 13:15:28 +0100

    Update GlobalVariable.js

--- TunaApp/webapp/utils/GlobalVariable.js
+++ TunaApp/webapp/utils/GlobalVariable.js
@@ -3,9 +3,9 @@ sap.ui.define([], function(){
 	return {
 		"MULTICHAIN_URL_PREFIX" : "https://maas-proxy.cfapps.sap.hana.ondemand.com/",
 		// "MULTICHAIN_URL_PREFIX" : "/multichainDestination/",
-		"MULTICHAIN_INSTANCE_ID": "a4431036-bb13-4a50-8ca9-f3cac8141383",
+		"MULTICHAIN_INSTANCE_ID": "a4431036-bb13-4a50-8ca9-f3cac8141111",
 		"STREAM_NAME": "root",
-		"API_KEY": "bUTkSJe9QFL5XnWW3mC64PCF4PHV6PNKeykqnVjcYEE4uCyrKdPBvCJuyFHrN8Co"
+		"API_KEY": "bUTkSJe9QFL5XnWW3mC64PCF4PHV6PNKeykqnVjcYEE4uCyrKdPBvCJuyFHr9999"
 		};	
 });
 
